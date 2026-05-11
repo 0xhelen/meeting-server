20a5b82fc5e79462939ccc65bb57f1d2d09a5846
0xhelen <eijazgdemidugd4y8@hotmail.com>
2026-05-11 18:04:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import { getApp } from "./app.js";
+import app from "./app.js";
 
 const PORT = Number(process.env.PORT) || 3001;
 
-const app = await getApp();
-
 export default app;
 
-// Local development only
 if (process.env.VERCEL !== "1") {
   app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
